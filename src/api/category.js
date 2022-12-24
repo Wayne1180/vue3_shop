981964daa89a07abfc0f1b832488ff3719aad4b4
wayne <xutenglongxutenglong@outlook.com>
2022-12-24 19:30:15 +0800

    完成了商品分类页面

--- src/api/category.js
+++ src/api/category.js
@@ -9,3 +9,26 @@ import request from '@/utils/request'
 export const findAllCategory = () => {
     return request('/home/category/head', 'get')
 }
+
+/* 
+    id - 顶级类目ID
+    获取顶级类目信息（children属性就是各个子分类）
+*/
+export const findTopCategory = (id) => {
+    return request('/category', 'get', { id })
+}
+
+/* 
+    id - 二级类目ID
+    获取二级类目的筛选条件数据
+*/
+export const findSubCategoryFilter = (id) => {
+    return request('/category/sub/filter', 'get', { id })
+}
+
+/* 
+    获取分类下的商品（带筛选条件）
+*/
+export const findSubCategoryGoods = (params) => {
+    return request('/category/goods/temporary', 'post', params)
+}
