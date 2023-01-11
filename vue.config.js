4261720e29b07426e494eb331da21bf5790fefdd
Wayne1180 <xutenglongxutenglong@outlook.com>
2023-01-11 14:41:38 +0800

    完成了商品详情页面的基础结构

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 const path = require('path')
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
 
+module.exports = {
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'less',
@@ -13,5 +11,22 @@ module.exports = defineConfig({
         path.join(__dirname, './src/assets/styles/mixins.less')
       ]
     }
-  }
-})
+  },
+  chainWebpack: config => {
+    // 图片加载
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => Object.assign(options, { limit: 10000 }))
+
+    // 开启IP域名访问
+    config.devServer.disableHostCheck(true)
+  },
+  configureWebpack: {
+    externals: {
+      qc: 'QC'
+    }
+  },
+  lintOnSave: false
+}
